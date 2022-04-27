d652140b2f2a4e751999983786676333b8df6235
MateBook <examletru@gmail.com>
2022-04-27 13:22:54 +0300

    first commit

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,8 +1,10 @@
+<script setup>
+</script>
+
 <template>
     <div h-full w-full>
         <slot />
-        <noscript>
-            <div><img src="https://mc.yandex.ru/watch/88635425" style="position:absolute; left:-9999px;" alt="" /></div>
-        </noscript>
+        
+        <img src="https://mc.yandex.ru/watch/88635425" style="position:absolute; left:-9999px;" alt="" />
     </div>
 </template>
